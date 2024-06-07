f78138e12c661e760739748f3d2b8a5dbf045116
hjwnu <tlstjr01@gmail.com>
2024-06-07 12:22:45 +0900

    init

---
+++ src/main/java/org/nullcom/nullcomback/NullcomBackApplication.java
@@ -0,0 +1,13 @@
+package org.nullcom.nullcomback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullcomBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NullcomBackApplication.class, args);
+  }
+
+}
